e9949fc266ff459df47219f3226c80af66986489
Hugo Montenegro <hugo@festivalgallery.es>
2018-02-05 12:28:52 +0100

    Create webhook.py

---
+++ webhook.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global lazout
+
+app = Flask(__name__)
+
+@app.route('/webhook', method = ['POST'])
+def webhook():
+    req = request.get_json(silent = True, force = True)
+    print('Request:')
+    print(json.dumps(req, indent = 4))
+    res = makeWebhookResult(req)
+    res = json.dumps(res, indent = 4)
+    print(res)
+    r = make_respons(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get('result').get('action') != 'Stock':
+        return {}
+    result = req.get('result')
+    parameters = result.get('parameters')
+    name = parameters.get('Stock')
+    speech = 'The Stock is' + name + ' lol'
+    print('Response123:')
+    print(speech)
+    return {
+        'speech': speech,
+        'displayText': speech,
+    }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
+
+
