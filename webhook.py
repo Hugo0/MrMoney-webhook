5bbf38e7d157f2588f492f5eb00655dd62ea6654
Hugo Montenegro <hugo@festivalgallery.es>
2018-02-05 12:53:55 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -6,7 +6,7 @@ from flask import make_response
 
 app = Flask(__name__)
 
-@app.route('/webhook', method = ['POST'])
+@app.route('/webhook', methods = ['POST'])
 def webhook():
     req = request.get_json(silent = True, force = True)
     print('Request:')
